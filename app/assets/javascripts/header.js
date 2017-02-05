1a2edb1272ebe5e384fb1938f2f4560538a70d1d
Ricardo Corral <ricardo.corral.castanon@gmail.com>
2017-02-04 20:44:19 -0600

    Changes to header

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,15 @@
+document.addEventListener("turbolinks:load", function() {	
+    $(".children-user").mouseover(function(){
+        $(".user-icon").css("color", "white"); 
+    });
+    $(".children-user").mouseleave(function(){
+       	$(".user-icon").css("color", "#bdc3c7");
+	});
+
+    $(".header-search").focusin(function(){
+        $(".header-submit").css("color", "white"); 
+    });
+    $(".header-search").focusout(function(){
+       	$(".header-submit").css("color", "#bdc3c7");
+	});
+})
