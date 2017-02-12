504f97195f879cd3cfc5c51ae7cd944c8f55be4b
Ricardo Corral <ricardo.corral.castanon@gmail.com>
2017-02-11 22:48:35 -0600

    Great changes to dashboard

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,18 @@
+  $(document).ready(function() {
+    $(".tabLink").each(function(){
+      $(this).click(function(){
+        localStorage.selectedTab = $(this).index() + 1;
+        tabeId = $(this).attr('id');
+        $(".tabLink").removeClass("activeLink");
+        $(this).addClass("activeLink");
+        $(".tabcontent").addClass("display-none");
+        $("#"+tabeId+"-1").removeClass("display-none")   
+        return false;     
+      });
+    });  
+      
+    // search for local storage
+    if (localStorage.selectedTab) {
+      $(".tabLink:eq(" + (localStorage.selectedTab - 1) + ")").click();
+    }
+  });
