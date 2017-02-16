20ca0ad9641ecb483e84ff0c1196ca47a2418e03
Ricardo Corral <ricardo.corral.castanon@gmail.com>
2017-02-16 11:10:35 -0600

    triying1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,9 +5,9 @@ class WelcomeController < ApplicationController
 		user = params[:user_search]
 		category = params[:category_search]
 
-		@articleas = Article.paginate(page: params[:articles_page], per_page: 5).search(params[:article_search])
-		@useras = User.paginate(page: params[:users_page], per_page: 1).search(user)
-		@categorieas = Category.paginate(page: params[:categories_page], per_page: 5).search(category)
+		@articls = Article.paginate(page: params[:articles_page], per_page: 5).search(params[:article_search])
+		@useres = User.paginate(page: params[:users_page], per_page: 1).search(user)
+		@categoris = Category.paginate(page: params[:categories_page], per_page: 5).search(category)
 		@article = Article.new
 	end
 end
