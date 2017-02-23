64d9e28ad670d181d602af28f9ef3a467fd07c35
Ricardo Corral <ricardo.corral.castanon@gmail.com>
2017-02-23 12:13:44 -0600

    Almost ended the jquery characters left

--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,7 +1,29 @@
 document.addEventListener("turbolinks:load", function() {
 	$('document').ready(function() {
+
+		var input = $('#myTextarea');
+		var popo = $(input).val().length;
+    	var count = 50 - popo
+    	$('#counter').text('Characters left: ' + count);
+
+        if (count < 0) {
+			$("#counter").css('color', 'red');
+			$(".save-article").addClass("grey"); 
+			$(".save-article").attr('disabled', 'disabled');
+			$( ".alert" ).text( "Si sobrepasas el límite de caracteres no podrás guardar el artículo" ); 
+		} else {
+		    $("#counter").css('color', 'green');
+		};
+
+		if (count < 6 && count > -1) {
+			$("#counter").css('color', 'orange'); 
+		} else {
+			$("#counter").css('color', 'none'); 
+		};
+
 		$('#myTextarea').keyup(function () {
-			var left = 50 - $(this).val().length;
+			var caca = $(this).val().length;
+			var left = 50 - caca
 			$('#counter').text('Characters left: ' + left);
 
 			if (left < 0) {
@@ -9,7 +31,7 @@ document.addEventListener("turbolinks:load", function() {
 			    $(".save-article").removeClass("blue");  
 			    $(".save-article").addClass("grey"); 
 			    $(".save-article").attr('disabled', 'disabled');
-			    $( ".alert" ).text( "Si sobrepasas el límite de caracteres no podrás crear el artículo" );
+			    $( ".alert" ).text( "Si sobrepasas el límite de caracteres no podrás guardar el artículo" );
 			} else { 
 		        $("#counter").css('color', 'green'); 
 			    $(".save-article").removeClass("grey");
@@ -23,5 +45,5 @@ document.addEventListener("turbolinks:load", function() {
 			    $("#counter").css('color', 'none'); 
 			};
 		});
-	}); 
+	});
 });
