0cd6a244e9aaa2e09579054652ebc0419623ee03
Ricardo Corral <ricardo.corral.castanon@gmail.com>
2017-02-23 00:00:04 -0600

    changes changes )8

--- app/assets/javascripts/ajax_loader.js
+++ app/assets/javascripts/ajax_loader.js
@@ -1,9 +1,9 @@
 document.addEventListener("turbolinks:load", function() {
 	$(document).ajaxStart(function() {
-	                $('.loading-indicator-container').fadeIn('fast');
-	                $('.loading-indicator').fadeIn('fast');
+	   	$('.loading-indicator-container').fadeIn('fast');
+	   	$('.loading-indicator').fadeIn('fast');
 	}).ajaxStop(function() {
-	                $('.loading-indicator-container').fadeOut('fast');
-	                $('.loading-indicator').fadeOut('fast');
+	    $('.loading-indicator-container').fadeOut('fast');
+	    $('.loading-indicator').fadeOut('fast');
 	});
-})
+});
