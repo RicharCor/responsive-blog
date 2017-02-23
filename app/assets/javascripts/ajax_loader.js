3492027826a5804b2e0a9595b7c628cb3e7f4fb7
Ricardo Corral <ricardo.corral.castanon@gmail.com>
2017-02-22 23:44:32 -0600

    experimenting sdd

--- app/assets/javascripts/ajax_loader.js
+++ app/assets/javascripts/ajax_loader.js
@@ -1,7 +1,9 @@
-$(document).ajaxStart(function() {
-                $('.loading-indicator-container').fadeIn('fast');
-                $('.loading-indicator').fadeIn('fast');
-}).ajaxStop(function() {
-                $('.loading-indicator-container').fadeOut('fast');
-                $('.loading-indicator').fadeOut('fast');
-});
+document.addEventListener("turbolinks:load", function() {
+	$(document).ajaxStart(function() {
+	                $('.loading-indicator-container').fadeIn('fast');
+	                $('.loading-indicator').fadeIn('fast');
+	}).ajaxStop(function() {
+	                $('.loading-indicator-container').fadeOut('fast');
+	                $('.loading-indicator').fadeOut('fast');
+	});
+})
