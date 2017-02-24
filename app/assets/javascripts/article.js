4b909c72991cdb08411e8ce88b70ae714a4fa32d
Ricardo Corral <ricardo.corral.castanon@gmail.com>
2017-02-24 12:16:55 -0600

    Editing article.js

--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,48 +1,52 @@
 document.addEventListener("turbolinks:load", function() {
 	$('document').ready(function() {
 
-		var input = $('#myTextarea');
-		var popo = $(input).val().length;
-    	var count = 50 - popo
-    	$('#counter').text('Characters left: ' + count);
+		// Esta funcion cuenta los caracteres en el input
+		var input = $('#article-title');
+		var characters = $(input).val().length;
+    	var count = 50 - characters
+    	var counter = $('#article-character-counter');
+    	$('#article-character-counter').text('Characters left: ' + count);
 
         if (count < 0) {
-			$("#counter").css('color', 'red');
+			counter.css('color', 'red');
 			$(".save-article").addClass("grey"); 
 			$(".save-article").attr('disabled', 'disabled');
 			$( ".alert" ).text( "Si sobrepasas el límite de caracteres no podrás guardar el artículo" ); 
 		} else {
-		    $("#counter").css('color', 'green');
+		    counter.css('color', 'green');
 		};
 
 		if (count < 6 && count > -1) {
-			$("#counter").css('color', 'orange'); 
+			counter.css('color', 'orange'); 
 		} else {
-			$("#counter").css('color', 'none'); 
+			counter.css('color', 'none'); 
 		};
 
-		$('#myTextarea').keyup(function () {
-			var caca = $(this).val().length;
-			var left = 50 - caca
-			$('#counter').text('Characters left: ' + left);
+		// Esta funcion resta a los caracteres faltantes cuando agregas un digito
+		$(input).keyup(function () {
+			var characters = $(this).val().length;
+			var left = 50 - characters
+			var counter = $('#article-character-counter');
+			$('#article-character-counter').text('Characters left: ' + left);
 
 			if (left < 0) {
-			    $("#counter").css('color', 'red');
+			    counter.css('color', 'red');
 			    $(".save-article").removeClass("blue");  
 			    $(".save-article").addClass("grey"); 
 			    $(".save-article").attr('disabled', 'disabled');
 			    $( ".alert" ).text( "Si sobrepasas el límite de caracteres no podrás guardar el artículo" );
 			} else { 
-		        $("#counter").css('color', 'green'); 
+		        counter.css('color', 'green'); 
 			    $(".save-article").removeClass("grey");
 			    $(".save-article").addClass("blue");  
 			    $(".save-article").removeAttr('disabled'); 
 			};
 
 			if (left < 6 && left > -1) {
-			    $("#counter").css('color', 'orange'); 
+			    counter.css('color', 'orange'); 
 			} else {
-			    $("#counter").css('color', 'none'); 
+			    counter.css('color', 'none'); 
 			};
 		});
 	});
