e9aff64663ad874d86d25e6bd0312137ff90acc2
Ricardo Corral <ricardo.corral.castanon@gmail.com>
2017-03-10 00:35:43 -0600

    dkljdakljkalsdl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,9 +75,6 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config.middleware.use ChatActionCable  
-  config.web_socket_server_url = "https://immense-dusk-74923.herokuapp.com//"  
-
   config.action_cable.allowed_request_origins = [
   'https://immense-dusk-74923.herokuapp.com/', 'https://immense-dusk-74923.herokuapp.com/']
 
