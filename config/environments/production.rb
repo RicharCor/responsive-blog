9789bd31c9257a980c963622fda7f72be3fc2915
Ricardo Corral <ricardo.corral.castanon@gmail.com>
2017-03-10 01:06:39 -0600

    rediss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,6 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config.action_cable.url = 'ws://immense-dusk-74923.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = ['https://immense-dusk-74923.herokuapp.com/']
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
